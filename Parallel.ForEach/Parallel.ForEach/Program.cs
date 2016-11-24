f035dd52195a3362e1740582b257f2806347f9ad
guven <guvennaslan@gmail.com>
2016-11-24 21:15:07 +0200

    Example completed

---
+++ Parallel.ForEach/Parallel.ForEach/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel.ForEach
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class City
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> _userList = new List<User>();
+            List<City> _cityList = new List<City>();
+            for (int i = 0; i < 50; i++)
+            {
+                _userList.Add(new User
+                {
+                    UserID = i,
+                    Name = i.ToString() + ". Name"
+                });
+                _cityList.Add(new City
+                {
+                    UserID = i,
+                    Name = i.ToString() + ". Name"
+                });
+            }
+            System.Threading.Tasks.Parallel.ForEach(_userList, (currentFile) =>
+            {
+                Console.WriteLine(currentFile.Name + ", " + _cityList.Where(x => x.UserID == currentFile.UserID).FirstOrDefault().Name);
+            });
+            Console.ReadKey();
+        }
+    }
+}
